68fe6f61fb755e4a817004ce9a276c683854ef7c
xiaodongli <xiaodongli@kias.re.kr>
2016-08-17 14:39:18 +0900

    updates

--- 105thick/scp.sh
+++ 105thick/scp.sh
@@ -1,3 +1,4 @@
 
-scp xiaodongli@annapurna.kias.re.kr:~/105thick/*.2pcf ./
-scp xiaodongli@annapurna.kias.re.kr:~/105thick/*.py ./
+#scp xiaodongli@annapurna.kias.re.kr:~/105thick/*.2pcf ./
+#scp xiaodongli@annapurna.kias.re.kr:~/105thick/*.py ./
+scp xiaodongli@annapurna.kias.re.kr:/home/xiaodongli/105thick/*shiftz*.2pcf ./
