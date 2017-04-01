5b85353dcaf20ddce85d0fc84403a83aab3676e3
xiaodongli <xiaodongli@kias.re.kr>
2017-04-02 08:20:02 +0900

    updates

--- 105thick/scp.sh
+++ 105thick/scp.sh
@@ -2,4 +2,8 @@
 #scp xiaodongli@annapurna.kias.re.kr:~/105thick/*.2pcf ./
 #scp xiaodongli@annapurna.kias.re.kr:~/105thick/*.py ./
 #scp xiaodongli@annapurna.kias.re.kr:/home/xiaodongli/105thick/*shiftz*.2pcf ./
-scp xiaodongli@newton.kias.re.kr:/home3/xiaodongli/cpfromanna/*shiftz*.2pcf ./
+#scp xiaodongli@newton.kias.re.kr:/home3/xiaodongli/cpfromanna/*shiftz*.2pcf ./
+#scp xiaodongli@newton.kias.re.kr:/home3/xiaodongli/cpfromanna/*diffmass*.2pcf ./
+#scp xiaodongli@newton.kias.re.kr:/home3/xiaodongli/cpfromanna2/*.2pcf ./
+scp xiaodongli@annapurna.kias.re.kr:~/105thick/J08.dat.z_*.mge3.000e11*ConstNbar*.2pcf ./
+
